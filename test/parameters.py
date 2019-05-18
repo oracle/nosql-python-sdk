2493b3bf7560333812f77438f93cdfa3e388e8f6
George Feinberg <gmf@dhcp-10-154-98-53.vpn.oracle.com>
2019-05-18 09:27:13 -0400

    Update Copyright notices in source. Rename THIRDPARTY.txt to THIRD_PARTY_LICENSES.txt

--- test/parameters.py
+++ test/parameters.py
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
 #
-# This software is licensed with the Universal Permissive License (UPL) version 1.0
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 #
 # Please see LICENSE.txt file included in the top-level directory of the
 # appropriate download for a copy of the license and additional information.
